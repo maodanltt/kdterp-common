82e3b8e7bb037e8a32b16199275814ade65198bf
maodan <maodan@163.com>
2021-02-20 18:06:53 +0800

    common工程初始导入

---
+++ src/main/java/com/tywh/kdterp/util/DbUtil.java
@@ -0,0 +1,64 @@
+package com.tywh.kdterp.util;
+
+import java.sql.*;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
+public class DbUtil {
+
+ //   private static ThreadLocal<Connection> threadLocal = new ThreadLocal<>();
+    private static Map<String, Connection> connMap = Collections.unmodifiableMap(new HashMap<>());
+
+    public static Connection getConnection() throws Exception {
+
+//        Connection conn = threadLocal.get();;
+//        if (conn == null) {
+//            Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+//            String url = "jdbc:sqlserver://*:1433;databasename=perpfx01";
+//            String user = "sa";
+//            String password = "*";
+//            conn = DriverManager.getConnection(url, user, password);
+//            threadLocal.set(conn);
+//        }i
+        Connection conn = connMap.get("conn");
+        if (conn == null) {
+            Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+            String url = "jdbc:sqlserver://114.215.124.181:1433;databasename=perpfx01";
+            String user = "sa";
+            String password = "tywh@2019";
+            conn = DriverManager.getConnection(url, user, password);
+            connMap.put("conn", conn);
+        }
+
+        return conn;
+    }
+
+    public static void close(Connection conn, Statement stmt, ResultSet rs) {
+
+        if (rs != null) {
+            try {
+                rs.close();
+            } catch (SQLException throwables) {
+                throwables.printStackTrace();
+            }
+        }
+
+        if (stmt != null) {
+            try {
+                stmt.close();
+            } catch (SQLException throwables) {
+                throwables.printStackTrace();
+            }
+        }
+
+        if (conn != null) {
+            try {
+                conn.close();
+//                threadLocal.remove();
+            } catch (SQLException throwables) {
+                throwables.printStackTrace();
+            }
+        }
+    }
+}
